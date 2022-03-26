e3a0d3d9e6e62d6567c2f5bdc7b3b1b7ad4402d6
giangvincent <giang.dlinh92@gmail.com>
2022-03-26 16:24:19 +0700

    Command: Start to learn this pattern

---
+++ command/Solution/VideoEditor.php
@@ -0,0 +1,30 @@
+<?php
+
+class VideoEditor {
+    private $contrast = 0.5;
+    private $text;
+
+    public function setText(String $text) {
+        $this->text = $text;
+    }
+
+    public function removeText() {
+        $this->text = "";
+    }
+
+    public function getContrast():float {
+        return $this->contrast;
+    }
+
+    public function setContrast(float $contrast) {
+        $this->contrast = $contrast;
+    }
+
+    // @Override
+    public function __toString() {
+        return "VideoEditor{" +
+                "contrast=" + $this->contrast +
+                ", text='" + $this->text + '\'' +
+                '}';
+    }
+}
