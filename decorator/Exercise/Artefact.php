f72932045a8e244bd09843748aec5241f1c42f89
giangvincent <giang.dlinh92@gmail.com>
2022-04-23 10:17:27 +0700

    Decorator pattern: start Decorator pattern

---
+++ decorator/Exercise/Artefact.php
@@ -0,0 +1,40 @@
+<?php
+
+class Artefact {
+    private $name;
+    private $hasError;
+    private $isMain;
+
+    public function Artefact(String $name) {
+        $this->name = $name;
+    }
+
+    public function render() {
+        // The current implementation is not easily extensible. If tomorrow we need
+        // to support other special markers, we have to come back and modify this class.
+        //
+        // For example, if the project is under source control, we need to highlight
+        // the artefacts that are changed but not committed to the repository with a
+        // special marker.
+        //
+        // Similarly, if an artefact is excluded from the project, we may want to
+        // make the icon look disabled or semi-transparent.
+        //
+        // Every time we need to support a new marker, we have to come back and modify
+        // this class. Over time, the code in this class gets more and more convoluted
+        // with several if statements and additional fields.
+
+        $errorIcon = $this->hasError ? "[Error]" : "";
+        $mainIcon = $this->isMain ? "[Main]" : "";
+
+        return sprintf("%s %s %s", $this->name, $errorIcon, $mainIcon);
+    }
+
+    public function setHasError(bool $hasError) {
+        $this->hasError = $hasError;
+    }
+
+    public function setMain(bool $main) {
+        $this->isMain = $main;
+    }
+}
